590b703eca14caeec0209d6ffb08d4c4aa38977b
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2023-04-07 20:29:35 -0400

    Create elseif.go

---
+++ elseif.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	playname := "jw"
+	if playname == "tay" {
+		fmt.Println("hi")
+	} else if playname == "technio" {
+		fmt.Println("hi technio")
+	} else {
+		fmt.Println("who even are you?")
+	}
+
+}
